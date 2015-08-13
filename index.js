8d6f75d73ad1c0ca94cdf0998c148a87526c63cc
Josh Gillies <github@joshgilli.es>
2015-08-13 17:37:16 +1000

    Add processing of url() paths

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+var path = require('path')
 var postcss = require('postcss')
+var parseCssUrls = require('css-url-parser')
 
-module.exports = postcss.plugin('postcss-mysource-path', mySourceFiles)
+module.exports = postcss.plugin('postcss-mysource-path', mySourcePath)
 
-function mySourceFiles (options) {
-  options = options || {}
-
+function mySourcePath () {
   return function (styles, result) {
-    // TODO: Make module! See below for a starting point
-    // https://github.com/postcss/postcss/blob/master/docs/api.md#resultmessages
+    styles.eachDecl(function (decl) {
+      if (decl.value && decl.value.indexOf('url(') > -1) {
+        var url = parseCssUrls(decl.value)[0]
+
+        decl.value = rewriteUrl(decl.value, url)
+      }
+    })
   }
+}
+
+function rewriteUrl (value, url) {
+  return value.replace(url, 'mysource_files/' + path.basename(url))
 }
--- test/test-default.js
+++ test/test-default.js
@@ -5,16 +5,16 @@ var path = require('path')
 var postcss = require('postcss')
 var mySourcePath = require('..')
 
-var source = fs.readFileSync(path.join(__dirname, './fixtures/example.css'), 'utf8').trim()
-var expected = fs.readFileSync(path.join(__dirname, 'fixtures/example.expected.css'), 'utf8').trim()
+var source = fs.readFileSync(path.join(__dirname, './fixtures/example.css'), 'utf8')
+var expected = fs.readFileSync(path.join(__dirname, 'fixtures/example.expected.css'), 'utf8')
 
-test('Default test', function (assert) {
+test('transform url() path to mysource_files', function (assert) {
   var pcss = postcss()
 
   pcss.use(mySourcePath())
 
   var actual = pcss.process(source).css
 
-  assert.equal(actual, expected)
+  assert.equal(actual, expected, 'path rewritten')
   assert.end()
 })
